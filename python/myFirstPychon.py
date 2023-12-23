819787e0d1146203599d0c37b63ffba3df260a7d
Ai-ming <401352263@qq.com>
2023-12-23 11:23:04 +0800

    v0.2

--- python/myFirstPychon.py
+++ python/myFirstPychon.py
@@ -1,4 +1,16 @@
+from typing import Any
+from wtforms.fields import simple
+
 class Foo(object):
 
-    def __init__(self) -> None:
+    def __init__(self,*args,**kwargs) :
+        super().__init__()  
         pass
+    def __call__(self, *args: Any, **kwds: Any) -> Any:
+        pass    
+    def __str__(self) -> str:
+        pass
+    def __new__(cls) -> Self:
+        pass
+
+
